3c244e02d28b324f5b87b78139f6940b4ac4b047
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-14 11:29:17 -0400

    Presists the todo to the DB, make spec pass

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,20 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
   def new
     @todo = Todo.new
+  end
+
+  def create
+    Todo.create(todo_parameters)
+    redirect_to root_path
+  end
+
+  private
+
+  def todo_parameters
+    params.require(:todo).permit(:title)
   end
 end
