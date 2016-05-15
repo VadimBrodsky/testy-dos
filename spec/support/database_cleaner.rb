927938d233704a820640795485560edf234ced22
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-15 12:45:41 -0400

    Add Database Cleaner and configuration

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,21 @@
+Rspec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  configure.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  configure.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  configure.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  configure.after(:eac) do
+    DatabaseCleaner.clean
+  end
+end
