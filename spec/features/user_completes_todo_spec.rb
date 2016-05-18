de192a219f2db600e5690596d6d0763bb7508ac8
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-18 11:43:39 -0400

    Extract method for todo creation in spec

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -4,10 +4,7 @@ feature 'User completes todo' do
   scenario 'successfully' do
     sign_in
 
-    click_on 'Add a new todo'
-    fill_in 'Title', with: 'Buy bread'
-    click_on 'Submit'
-
+    create_todo('Buy bread')
     click_on 'Mark complete'
 
     expect(page).to have_css '.todos li.completed', text: 'Buy bread'
